3f58f68e6303a03a0ba6bb5b8e1a9d7aebc63764
Idriss <you@example.com>
2023-12-30 14:35:56 +0100

    fourth commit

--- src/App.js
+++ src/App.js
@@ -1,49 +1,57 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { useState } from 'react'
-import Navbar from './Components/Navbar'
-import Home from './Pages/Home'
-import Login from './Pages/Login'
-import SignUp from './Pages/SignUp'
-import Sell from './Pages/Sell'
-import AboutUs from './Pages/AboutUs'
-import Payment from './Pages/Payment'
-import MyItems from './Pages/MyItems'
-import MyBids from './Pages/MyBids'
-import Footer from './Components/Footer'
-import ScrollToTop from './ScrollToTop'
-import ProductDetails from './Pages/ProductDetails'
-import UserContext from './UserContext'
-import ProductContext from './ProductContext'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import Navbar from "./Components/Navbar";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import SignUp from "./Pages/SignUp";
+import Sell from "./Pages/Sell";
+import AboutUs from "./Pages/AboutUs";
+import Payment from "./Pages/Payment";
+import MyItems from "./Pages/MyItems";
+import MyBids from "./Pages/MyBids";
+import Footer from "./Components/Footer";
+import ScrollToTop from "./ScrollToTop";
+import ProductDetails from "./Pages/ProductDetails";
+import UserContext from "./UserContext";
+import ProductContext from "./ProductContext";
 
 const App = () => {
   const [username, setUsername] = useState(""); //use usestate and usercontext to pass the username between elements
+  const [Solde, setSolde] = useState("");
   const [selectedProduct, setSelectedProduct] = useState(null); //use usestate and productcontext to pass the selected product between elements
 
   return (
-    <div className='App'>
-        <Router>
-            <ScrollToTop/>
-            <UserContext.Provider value={{ username, setUsername }}>
-            <ProductContext.Provider value={{ selectedProduct, setSelectedProduct }}>
-              <Navbar/>
-              <Routes>
-                  <Route path='/BidWise/' element={<Home/>}/>
-                  <Route path='/BidWise/Login/' element={<Login/>}/>
-                  <Route path='/BidWise/SignUp/' element={<SignUp/>}/>
-                  <Route path='/BidWise/Sell/' element={<Sell/>}/>
-                  <Route path='/BidWise/AboutUs/' element={<AboutUs/>}/>
-                  <Route path='/BidWise/Payment/' element={<Payment/>}/>
-                  <Route path='/BidWise/MyItems/' element={<MyItems/>}/>
-                  <Route path='/BidWise/MyBids/' element={<MyBids/>}/>
-                  <Route path='/BidWise/ProductDetails/:id' element={<ProductDetails/>}/>
-              </Routes>
-            </ProductContext.Provider>
-            </UserContext.Provider>
-            <Footer/>
-        </Router>
+    <div className="App">
+      <Router>
+        <ScrollToTop />
+        <UserContext.Provider
+          value={{ username, setUsername, Solde, setSolde }}
+        >
+          <ProductContext.Provider
+            value={{ selectedProduct, setSelectedProduct }}
+          >
+            <Navbar />
+            <Routes>
+              <Route path="/BidWise/" element={<Home />} />
+              <Route path="/BidWise/Login/" element={<Login />} />
+              <Route path="/BidWise/SignUp/" element={<SignUp />} />
+              <Route path="/BidWise/Sell/" element={<Sell />} />
+              <Route path="/BidWise/AboutUs/" element={<AboutUs />} />
+              <Route path="/BidWise/Payment/" element={<Payment />} />
+              <Route path="/BidWise/MyItems/" element={<MyItems />} />
+              <Route path="/BidWise/MyBids/" element={<MyBids />} />
+              <Route
+                path="/BidWise/ProductDetails/:id"
+                element={<ProductDetails />}
+              />
+            </Routes>
+          </ProductContext.Provider>
+        </UserContext.Provider>
+        <Footer />
+      </Router>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/Pages/MyItems.jsx
+++ src/Pages/MyItems.jsx
@@ -4,80 +4,81 @@ import { Link } from "react-router-dom";
 import { auth, db, storage } from "../firebase";
 import { collection, getDocs } from "@firebase/firestore";
 import { ref, getDownloadURL } from "@firebase/storage";
-import MyItem from '../Components/MyItem';
+import MyItem from "../Components/MyItem";
 
-import '../Styles/MyItems.css';
-import empty from '../Images/empty.png';
+import "../Styles/MyItems.css";
+import empty from "../Images/empty.png";
 
 const MyItems = () => {
-    const { username } = useContext(UserContext);
-    const [productList, setProductList] = useState([]);
+  const { username } = useContext(UserContext);
+  const [productList, setProductList] = useState([]);
 
-    const fetchData = async () => {
-        const collectionRef = collection(db, "itemdetails");
+  const fetchData = async () => {
+    const collectionRef = collection(db, "itemdetails");
+
+    try {
+      const snapshot = await getDocs(collectionRef);
+      const productsData = [];
+
+      for (const doc of snapshot.docs) {
+        const data = doc.data();
+        const imageRef = ref(storage, data.imageUrl);
 
         try {
-        const snapshot = await getDocs(collectionRef);
-        const productsData = [];
+          const imageUrl = await getDownloadURL(imageRef);
 
-        for (const doc of snapshot.docs) {
-            const data = doc.data();
-            const imageRef = ref(storage, data.imageUrl);
-
-            try {
-            const imageUrl = await getDownloadURL(imageRef);
-
-            // Filter items where product.owner === username
-            if (data.ItemOwner === username) {
-                productsData.push({
-                name: data.itemName,
-                description: data.itemShortDescription,
-                price: data.TOPBID,
-                enddate: data.endDate,
-                image: imageUrl,
-                owner: data.ItemOwner,
-                });
-            }
-            } catch (error) {
-            console.error(
-                `Error getting download URL for image: ${data.imageUrl}`,
-                error
-            );
-            }
-        }
-
-        setProductList(productsData);
+          // Filter items where product.owner === username
+          if (data.ItemOwner === username) {
+            productsData.push({
+              name: data.itemName,
+              description: data.itemShortDescription,
+              price: data.TOPBID,
+              enddate: data.endDate,
+              image: imageUrl,
+              owner: data.ItemOwner,
+            });
+          }
         } catch (error) {
-        console.error("Error fetching data from Firestore: ", error);
+          console.error(
+            `Error getting download URL for image: ${data.imageUrl}`,
+            error
+          );
         }
-    };
+      }
 
-    useEffect(() => {
-        fetchData();
-    }, []);
+      setProductList(productsData);
+    } catch (error) {
+      console.error("Error fetching data from Firestore: ", error);
+    }
+  };
 
-    return (
-        <div className='my-items'>
-            <h1 id="myitems-headline">My published bids</h1>
-            <div children='my-items-container'>
-                {productList.map((product, index) => (
-                    <MyItem key={index} product={product} />
-                ))}
-                {productList.length === 0 && (
-                    <div className="empty">
-                        <div className='empty-img-div'>
-                            <img src={empty} alt='no published items' id='empty-img'/>
-                            <p className="empty-text">No items published yet.</p>
-                        </div>
-                        <div className='add-btn-div'>
-                            <p>Publish your first bid:</p>
-                            <Link to='/BidWise/Sell/'><button id='add-item-btn'>Add Item</button></Link>
-                        </div>
-                    </div>
-                )}
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return (
+    <div className="my-items">
+      <h1 id="myitems-headline">My published bids</h1>
+      <div className="my-items-container">
+        {productList.map((product, index) => (
+          <MyItem key={index} product={product} />
+        ))}
+        {productList.length === 0 && (
+          <div className="empty">
+            <div className="empty-img-div">
+              <img src={empty} alt="no published items" id="empty-img" />
+              <p className="empty-text">No items published yet.</p>
             </div>
-        </div>
-    )
-}
+            <div className="add-btn-div">
+              <p>Publish your first bid:</p>
+              <Link to="/BidWise/Sell/">
+                <button id="add-item-btn">Add Item</button>
+              </Link>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default MyItems;
